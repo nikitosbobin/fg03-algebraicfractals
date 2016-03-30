5b7f62363e777043d0de20acdd6a86799bb97821
nikit <nikitosbobin@gmail.com>
2016-03-30 23:02:50 +0500

    Полность сделал задание 3.1

--- HomeWork/script.js
+++ HomeWork/script.js
@@ -1,5 +1,16 @@
 var canvas;
 var ctx;
+var leftP = -2;
+var rightP = 2;
+var topP = 2;
+var bottomP = -2;
+var drawingType = 0;
+var iterations = 10;
+
+function getComplexPoint(i, j) {
+    return { x: i*(rightP - leftP)/(canvas.width - 1) + leftP,
+             y: j*(bottomP - topP)/(canvas.height - 1) + topP };
+}
 
 function load() {
     canvas = document.getElementById("mCanvas");
@@ -7,36 +18,135 @@ function load() {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     canvas.onmousedown = mouseDown;
+    canvas.onmouseup = mouseUp;
+    canvas.onmousemove = mouseMove;
+    drawFractal();
+}
+
+function drawFractal() {
     var imageData = ctx.createImageData(canvas.width, canvas.height);
     for (var i = 0; i < canvas.width; ++i)
         for (var j = 0; j < canvas.height; ++j) {
-            var p = getComplexPoint(i, j, 2, -2, 2, -2, canvas.width, canvas.height);
-            var attr = getAttraction(p, 40);
-            switch (attr.root) {
-                case 0: imageData.data[4 * (i + canvas.width * j) + 0] = 255;
-                        imageData.data[4 * (i + canvas.width * j) + 1] = 0; 
-                        imageData.data[4 * (i + canvas.width * j) + 2] = 0;
-                        imageData.data[4 * (i + canvas.width * j) + 3] = 255;
-                        break;
-                case 1: imageData.data[4 * (i + canvas.width * j) + 0] = 0;
-                        imageData.data[4 * (i + canvas.width * j) + 1] = 255; 
-                        imageData.data[4 * (i + canvas.width * j) + 2] = 0;
-                        imageData.data[4 * (i + canvas.width * j) + 3] = 255;
-                        break;
-                case 2: imageData.data[4 * (i + canvas.width * j) + 0] = 0;
-                        imageData.data[4 * (i + canvas.width * j) + 1] = 0; 
-                        imageData.data[4 * (i + canvas.width * j) + 2] = 255;
-                        imageData.data[4 * (i + canvas.width * j) + 3] = 255;
-                        break;
+            var p = getComplexPoint(i, j);
+            var attr = getAttraction(p, iterations);
+            switch (drawingType) {
+                case 0: classicDrawing(i, j, attr.root, imageData); break;
+                case 1: levelDrawing(attr.count, i, j, imageData); break;
+                case 2: zebraDrawing(attr.count, i, j, imageData); break;
+                case 3: hybridDrawing(attr.count, attr.root, i, j, imageData); break;
             }
         }
     ctx.putImageData(imageData, 0, 0);
 }
 
+function hybridDrawing(n, root, x, y, imageData) {
+    var pixel;
+    var brightness = iterations > 1 ? 255*n/(iterations - 1) : 255;
+    switch (root) {
+        case 0: pixel = { r: brightness, g: 0,   b: 0,   a: 255 }; break;
+        case 1: pixel = { r: 0,   g: brightness, b: 0,   a: 255 }; break;
+        case 2: pixel = { r: 0,   g: 0,   b: brightness, a: 255 }; break;
+    }
+    fillPoint(imageData, x, y, pixel);
+}
+
+function onItChange(obj) {
+    iterations = obj.value;
+    drawFractal();
+}
+
+function zebraDrawing(n, x, y, imageData) {
+    var colors = n % 2 == 0 ? 0 : 255;
+    fillPoint(imageData, x, y, { r: colors, g: colors, b: colors, a: 255 });
+}
+
+function levelDrawing(n, x, y, imageData) {
+    var brightness = iterations > 1 ? 255*n/(iterations - 1) : 255;
+    fillPoint(imageData, x, y, { r: brightness, g: brightness, b: brightness, a: 255 });
+}
+
+function classicDrawing(x, y, root, imageData) {
+    var pixel;
+    switch (root) {
+        case 0: pixel = { r: 255, g: 0,   b: 0,   a: 255 }; break;
+        case 1: pixel = { r: 0,   g: 255, b: 0,   a: 255 }; break;
+        case 2: pixel = { r: 0,   g: 0,   b: 255, a: 255 }; break;
+    }
+    fillPoint(imageData, x, y, pixel);
+}
+
+function fillPoint(imageData, x, y, pixel) {
+    imageData.data[4 * (x + canvas.width * y) + 0] = pixel.r;
+    imageData.data[4 * (x + canvas.width * y) + 1] = pixel.g; 
+    imageData.data[4 * (x + canvas.width * y) + 2] = pixel.b;
+    imageData.data[4 * (x + canvas.width * y) + 3] = pixel.a;
+}
+
+var isMouseDown = false;
+var startPoint = null;
+var endPoint = null;
+var lastData = null;
+var statesStack = [];
 function mouseDown(e) {
-    var x = e.pageX - 8;
-    var y = e.pageY - 8;
-    console.log(x, y);
+    lastData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    isMouseDown = true;
+    startPoint = { x: e.pageX - 8, y: e.pageY - 8 };
+}
+
+function mouseMove(e) {
+    if (isMouseDown) {
+        var x = e.pageX - 8;
+        var y = e.pageY - 8;
+        ctx.strokeStyle = "#000";
+        ctx.strokeWidth = 2;
+        var dx = Math.abs(x - startPoint.x);
+        var dy = Math.abs(y - startPoint.y);
+        var summaryDelta = 0;
+        if (dx > dy) {
+            summaryDelta = dx;
+            endPoint = { x: startPoint.x + dx, y: startPoint.y + dx};
+        }else {
+            summaryDelta = dy;
+            endPoint = { x: startPoint.x + dy, y: startPoint.y + dy};
+        }
+        ctx.putImageData(lastData, 0, 0);
+        ctx.strokeRect(startPoint.x, startPoint.y, summaryDelta, summaryDelta);
+    }
+}
+
+function drawingTypeChange(obj) {
+    drawingType = parseInt(obj.value);
+    drawFractal();
+}
+
+function mouseUp(e) {
+    isMouseDown = false;
+    var r = endPoint.x;
+    var b = endPoint.y;
+    var l = startPoint.x;
+    var t = startPoint.y;
+    var tmp1 = getComplexPoint(l, t);
+    var tmp2 = getComplexPoint(r, b);
+    statesStack.push({ l: leftP, r: rightP, t: topP, b: bottomP });
+    leftP = tmp1.x;
+    topP = tmp1.y;
+    rightP = tmp2.x;
+    bottomP = tmp2.y;
+    drawFractal();
+    startPoint = null;
+    lastData = null;
+}
+
+function unzoom() {
+    if (statesStack.length > 0) {
+        var tmp = statesStack.pop();
+        leftP = tmp.l;
+        topP = tmp.t;
+        rightP = tmp.r;
+        bottomP = tmp.b;
+        drawFractal();
+    }
+    
 }
 
 var roots = [
@@ -66,25 +176,4 @@ function getNextRealPoint(point) {
 function isPointInSurrounding(x, y, root) {
     var epsylon = 0.0001;
     return Math.abs(x - root.x) <= epsylon && Math.abs(y - root.y) <= epsylon;
-}
-
-function fillPoint(x, y, color) {
-    switch(color) {
-        case "red": ctx.fillStyle = "#f44336";
-            break;
-        case "green": ctx.fillStyle = "#4caf50";
-            break;
-        case "blue": ctx.fillStyle = "#2196f3";
-            break;
-    }
-    ctx.fillRect(x, y, 1, 1);
-}
-
-function getComplexPoint(i, j, right, left, top, bottom, width, height) {
-    return new Point(i*(right - left)/(width - 1) + left, j*(bottom - top)/(height - 1) + top)
-}
-
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
 }
